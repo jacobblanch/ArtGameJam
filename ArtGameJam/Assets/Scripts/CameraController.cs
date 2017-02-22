dcc50afc9c2e7c8be4f9527c48e80ad94a1d6d6b
Jacob Blanch <1006027@student.sae.edu.au>
2017-02-23 03:19:50 +1000

    Wow do it this time pls

---
+++ ArtGameJam/Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    public GameObject endPos;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        CameraMover();
+        Debug.Log(Time.timeSinceLevelLoad);
+    }
+
+    public void CameraMover()
+    {
+        if (Time.timeSinceLevelLoad > 15)
+        transform.position = Vector3.MoveTowards(transform.position, endPos.transform.position, 0.01f);
+    }
+}
--- ArtGameJam/Assets/Scripts/PlayerController.cs
+++ ArtGameJam/Assets/Scripts/PlayerController.cs
@@ -4,53 +4,31 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour {
-    public float thrust;
-    public Rigidbody rb;
-    float InvisibleTime;
-    public GameObject anchor;
 
 	// Use this for initialization
 	void Start ()
     {
-        rb = GetComponent<Rigidbody>();
+
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        PlayerMovement();
-        PlayerAway();
+        PlayerMovement();   
 	}
 
     public void PlayerMovement()
     {
-        if (Input.GetKey("w"))
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(ray, out hitInfo))
         {
-            rb.AddForce(transform.forward * thrust);
-        }
-        if (Input.GetKey("s"))
-        {
-            rb.AddForce(transform.forward * -thrust);
-        }
-        if (Input.GetKey("a"))
-        {
-            rb.AddForce(transform.right * -thrust);
-        }
-        if (Input.GetKey("d"))
-        {
-            rb.AddForce(transform.right * thrust);
+            transform.position = hitInfo.point + new Vector3(0,2,0);
         }
         if (Input.GetKey("escape"))
         {
             Application.Quit();
-        }
-    }
-
-    public void PlayerAway()
-    {
-        if (Vector3.Distance(transform.position, anchor.transform.position) > 35)
-        {
-            transform.position = anchor.transform.position;
         }
     }
 }
